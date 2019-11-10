aa7509f3fe76eaffa00fa861f1db81eb57abe4e8
zhukeming123 <zhukeming123@qq.com>
2019-11-11 07:27:58 +0800

    update lianjia village

--- house_spider/spiders/lianjia.py
+++ house_spider/spiders/lianjia.py
@@ -4,7 +4,7 @@ from scrapy import Selector
 import json
 import re
 
-from house_spider.items import LianjiaVillageItem
+from house_spider.items import LianjiaVillageItem, LianjiaHouseItem
 
 
 class LianjiaSpider(scrapy.Spider):
@@ -63,7 +63,7 @@ class LianjiaSpider(scrapy.Spider):
             pass
 
         item = LianjiaVillageItem()
-        item['id'] = village_url.replace('https://cq.lianjia.com/xiaoqu/', '').replace('/', '')
+        item['id'] = village_url.replace(self.base_url + '/xiaoqu/', '').replace('/', '')
         item['name'] = sel.css('.detailHeader .detailTitle::text').extract_first()
         item['address'] = sel.css('.detailHeader .detailDesc::text').extract_first()
         item['latitude'] = latitude
@@ -76,4 +76,57 @@ class LianjiaSpider(scrapy.Spider):
         item['developers'] = sel.css('.xiaoquInfo .xiaoquInfoItem:nth-child(5) .xiaoquInfoContent::text').extract_first()
         item['buildings'] = sel.css('.xiaoquInfo .xiaoquInfoItem:nth-child(6) .xiaoquInfoContent::text').extract_first()
         item['total_house'] = sel.css('.xiaoquInfo .xiaoquInfoItem:nth-child(7) .xiaoquInfoContent::text').extract_first()
+        yield item
+
+        # 小区房源 https://cq.lianjia.com/ershoufang/c3620038190566370/
+        url = self.base_url + "/ershoufang/c" + item['id'] + "/"
+        yield scrapy.Request(url=url, callback=self.parse_house_list, meta={"ref": url})
+
+    def parse_house_list(self, response):
+        sel = Selector(response)
+        total = sel.css('.resultDes .total span::text').extract_first()
+        total = int(total)
+        if total > 0:
+            # 提取房源链接
+            links = sel.css(".sellListContent .title .LOGCLICKDATA::attr(href)").extract()
+            for link in links:
+                yield scrapy.Request(url=link, callback=self.parse_house_detail)
+
+            # page
+            page_data = sel.css(".house-lst-page-box::attr(page-data)").extract_first()
+            page_data = json.loads(page_data)
+            if page_data['curPage'] < page_data['totalPage']:
+                url = response.meta["ref"] + 'pg' + str(page_data['curPage'] + 1)
+                yield scrapy.Request(url=url, callback=self.parse_house_list, meta=response.meta)
+
+    def parse_house_detail(self, response):
+        sel = Selector(response)
+
+        item = LianjiaHouseItem()
+        item['房屋Id'] = response.url.replace(self.base_url + '/ershoufang/', '').replace('.html', '')
+        item['标题'] = sel.css('.title-wrapper .title .main::text').extract_first()
+        item['售价'] = sel.css('.overview .content .price .total::text').extract_first()
+        item['小区'] = sel.css('.overview .content .aroundInfo .communityName a.info::text').extract_first()
+        item['小区ID'] = sel.css('.overview .content .aroundInfo .communityName a.info::attr(href)').extract_first().replace('/xiaoqu/', '').replace('/', '')
+        item['房屋户型'] = sel.css('#introduction .base .content ul li:nth-child(1)::text').extract_first()
+        item['所在楼层'] = sel.css('#introduction .base .content ul li:nth-child(2)::text').extract_first()
+        item['建筑面积'] = sel.css('#introduction .base .content ul li:nth-child(3)::text').extract_first()
+        item['户型结构'] = sel.css('#introduction .base .content ul li:nth-child(4)::text').extract_first()
+        item['套内面积'] = sel.css('#introduction .base .content ul li:nth-child(5)::text').extract_first()
+        item['建筑类型'] = sel.css('#introduction .base .content ul li:nth-child(6)::text').extract_first()
+        item['房屋朝向'] = sel.css('#introduction .base .content ul li:nth-child(7)::text').extract_first()
+        item['建筑结构'] = sel.css('#introduction .base .content ul li:nth-child(8)::text').extract_first()
+        item['装修情况'] = sel.css('#introduction .base .content ul li:nth-child(9)::text').extract_first()
+        item['梯户比例'] = sel.css('#introduction .base .content ul li:nth-child(10)::text').extract_first()
+        item['配备电梯'] = sel.css('#introduction .base .content ul li:nth-child(11)::text').extract_first()
+        item['产权年限'] = sel.css('#introduction .base .content ul li:nth-child(12)::text').extract_first()
+        item['挂牌时间'] = sel.css('#introduction .transaction .content ul li:nth-child(1) span:nth-child(2)::text').extract_first()
+        item['交易权属'] = sel.css('#introduction .transaction .content ul li:nth-child(2) span:nth-child(2)::text').extract_first()
+        item['上次交易'] = sel.css('#introduction .transaction .content ul li:nth-child(3) span:nth-child(2)::text').extract_first()
+        item['房屋用途'] = sel.css('#introduction .transaction .content ul li:nth-child(4) span:nth-child(2)::text').extract_first()
+        item['房屋年限'] = sel.css('#introduction .transaction .content ul li:nth-child(5) span:nth-child(2)::text').extract_first()
+        item['产权所属'] = sel.css('#introduction .transaction .content ul li:nth-child(6) span:nth-child(2)::text').extract_first()
+        item['抵押信息'] = sel.css('#introduction .transaction .content ul li:nth-child(7) span:nth-child(2)::attr(title)').extract_first()
+        item['房本备件'] = sel.css('#introduction .transaction .content ul li:nth-child(8) span:nth-child(2)::text').extract_first()
+
         yield item
